357b3aa9e20a7bdb47d52d5fe97b5ea8705031b3
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2026-01-07 15:18:09 +0530

    Payment Term Crud Created

--- src/routes/paymentTerm.ts
+++ src/routes/paymentTerm.ts
@@ -4,7 +4,11 @@ import { paymentTermController } from "../controllers";
 const router = express.Router();
 
 router.get("/all", paymentTermController.getAllPaymentTerm);
+router.post("/add", paymentTermController.addPaymentTerm);
+router.put("/edit", paymentTermController.editPaymentTerm);
+router.delete("/:id", paymentTermController.deletePaymentTermById);
+router.get("/:id", paymentTermController.getPaymentTermById);
+
 router.get("/dropdown", paymentTermController.getPaymentTermDropdown);
 
 export const paymentTermRouter = router;
-
