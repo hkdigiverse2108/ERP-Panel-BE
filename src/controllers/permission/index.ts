808acd6909ab2dc27a34391c29dd07f47c430c3b
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2026-01-23 19:29:10 +0530

    Add user permission retrieval by userId and child module details endpoint
    - Implemented a new endpoint to fetch user permissions based on userId, including search functionality for module attributes.
    - Enhanced the aggregation pipeline to include parent module details and structured the response to include user-specific permissions.
    - Added a new route for retrieving child module permissions, improving the overall permission management capabilities.

--- src/controllers/permission/index.ts
+++ src/controllers/permission/index.ts
@@ -53,6 +53,68 @@ export const get_permission_by_userId = async (req, res) => {
         }
 
         match.isActive = true;
+
+        let moduleData = await moduleModel.aggregate([
+            { $match: match },
+            {
+                $lookup: {
+                    from: "modules",
+                    let: { tabId: '$parentId' },
+                    pipeline: [{ $match: { $expr: { $and: [{ $eq: ['$_id', '$$tabId'] },], }, } }],
+                    as: "modules"
+                }
+            },
+            {
+                $unwind: { path: "$modules", preserveNullAndEmptyArrays: true }
+            },
+        ])
+        let newUserPermissionData = [];
+        moduleData?.forEach(item => {
+            let newObj = {
+                parentTab: item.modules !== null ? item.modules : {},
+                view: false,
+                add: false,
+                edit: false,
+                delete: false,
+            };
+
+            let permission = userPermissionData?.find(item2 => item2.userId.toString() == userId.toString() && item2.moduleId.toString() == item._id.toString() && item.isActive == true);
+            if (permission) {
+                newObj.view = permission.view;
+                newObj.add = permission.add;
+                newObj.edit = permission.edit;
+                newObj.delete = permission.delete;
+            }
+            newUserPermissionData.push({ ...item, ...newObj });
+        });
+
+        return res.status(HTTP_STATUS.OK).json(new apiResponse(HTTP_STATUS.OK, responseMessage?.getDataSuccess("user permissions"), newUserPermissionData.sort((a, b) => a.number - b.number), {}));
+    } catch (error) {
+        console.log(error);
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json(new apiResponse(HTTP_STATUS.INTERNAL_SERVER_ERROR, responseMessage?.internalServerError, {}, error));
+    }
+};
+
+export const get_permission_by_userId_child = async (req, res) => {
+    reqInfo(req)
+    try {
+        let { error, value } = getPermissionSchema.validate(req.query);
+        if (error) return res.status(HTTP_STATUS.BAD_REQUEST).json(new apiResponse(HTTP_STATUS.BAD_REQUEST, error?.details[0]?.message, {}, {}));
+
+        let { userId, search } = value, match: any = {};
+
+        let userPermissionData = await getData(permissionModel, { userId: new ObjectId(userId) }, {}, {});
+        if (!userPermissionData) return res.status(HTTP_STATUS.METHOD_NOT_ALLOWED).json(new apiResponse(HTTP_STATUS.METHOD_NOT_ALLOWED, responseMessage.getDataNotFound("user permissions"), {}, {}));
+
+        if (search) {
+            match.$or = [
+                { tabName: { $regex: search, $options: 'si' } },
+                { displayName: { $regex: search, $options: 'si' } },
+                { tabUrl: { $regex: search, $options: 'si' } },
+            ];
+        }
+
+        match.isActive = true;
         match.isDeleted = false;
 
         let moduleData = await moduleModel.aggregate([
--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post("/edit", permissionController.edit_permission_by_id)
 router.get("/details", permissionController.get_permission_by_userId)
+router.get("/child/details", permissionController.get_permission_by_userId_child)
 
 export const permissionRoute = router
