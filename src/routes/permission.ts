b71e6023720bda7d4feb5591c792f0d7c4158566
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2026-01-24 14:33:29 +0530

    Change POST to PUT method for editing permissions in the permission route

--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -3,7 +3,7 @@ import { permissionController } from "../controllers";
 
 const router = Router();
 
-router.post("/edit", permissionController.edit_permission_by_id)
+router.put("/edit", permissionController.edit_permission_by_id)
 router.get("/details", permissionController.get_permission_by_userId)
 router.get("/child/details", permissionController.get_permission_by_userId_child)
 
