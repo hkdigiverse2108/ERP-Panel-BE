e2691a00e5e3d7f9766a39aeadb910b6770b0288
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2026-02-17 17:39:12 +0530

    Coupon Dropdown Api Created

--- src/routes/coupon.ts
+++ src/routes/coupon.ts
@@ -4,6 +4,7 @@ import { couponController } from "../controllers";
 const router = express.Router();
 
 router.get("/all", couponController.getAllCoupon);
+router.get("/dropdown", couponController.getCouponDropdown);
 router.post("/add", couponController.addCoupon);
 router.post("/apply", couponController.applyCoupon);
 router.post("/remove", couponController.removeCoupon);
