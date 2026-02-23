0daaaa50c6d6d2c8b6c1ec0851fa05435b99f8fa
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-02-23 14:08:18 +0530

    Social Media Links Crud

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,8 @@ import { authController } from "../controllers";
 
 const router = Router();
 
+console.log("authRoute");
+
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/reset-password", authController.resetPassword);
