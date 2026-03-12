a9d19428c8ea7515f8c2b0b843477bc732f1fd39
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-03-12 17:31:04 +0530

    updated the population error in the cash register.

--- src/controllers/posCashRegister/index.ts
+++ src/controllers/posCashRegister/index.ts
@@ -341,7 +341,7 @@ export const getAllPosCashRegister = async (req, res) => {
       populate: [
         { path: "branchId", select: "name" },
         { path: "companyId", select: "name" },
-        { path: "salesManId", select: "name" },
+        { path: "salesManId", select: "fullName" },
         { path: "bankAccountId", select: "name" },
       ],
     };
