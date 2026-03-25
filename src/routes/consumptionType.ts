20a3eec4e2ef484a66ef54fb36a6a0affbeaaf96
hk-krish <krishhkdigiverse@gmail.com>
2026-03-25 08:59:58 +0530

    feat: Add API routes for consumption type management.

--- src/routes/consumptionType.ts
+++ src/routes/consumptionType.ts
@@ -4,10 +4,10 @@ import * as consumptionTypeController from "../controllers/consumptionType";
 const router = Router();
 
 router.post("/add", consumptionTypeController.addConsumptionType);
-router.post("/edit", consumptionTypeController.editConsumptionType);
-router.delete("/delete/:id", consumptionTypeController.deleteConsumptionType);
-router.get("/get-one/:id", consumptionTypeController.getOneConsumptionType);
-router.get("/get-all", consumptionTypeController.getAllConsumptionType);
+router.put("/edit", consumptionTypeController.editConsumptionType);
+router.delete("/:id", consumptionTypeController.deleteConsumptionType);
+router.get("/:id", consumptionTypeController.getOneConsumptionType);
+router.get("/all", consumptionTypeController.getAllConsumptionType);
 router.get("/dropdown", consumptionTypeController.consumptionTypeDropDown);
 
 export { router as consumptionTypeRouter };
