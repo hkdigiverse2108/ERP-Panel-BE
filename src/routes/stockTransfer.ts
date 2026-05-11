b1a99fa4a6bd6dde3325bcf5b32af4681a4d1d25
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-05-11 10:17:06 +0530

    stock transfer update.

--- src/routes/stockTransfer.ts
+++ src/routes/stockTransfer.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.post("/request", stockTransferController.requestStockTransfer);
 router.post("/approve", stockTransferController.approveStockTransfer);
+router.post("/dispatch", stockTransferController.dispatchStockTransfer);
 router.post("/confirm-receipt", stockTransferController.confirmReceiptStockTransfer);
 router.post("/reject", stockTransferController.rejectStockTransfer);
 // router.post("/cancel", stockTransferController.cancelStockTransfer);
