7676a64fbe12207f4a40a7874f9a3483f72a6fef
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-05-15 14:16:37 +0530

    update

--- src/routes/reportFormat.ts
+++ src/routes/reportFormat.ts
@@ -10,7 +10,7 @@ router.get("/get-branch-config", reportFormatController.getBranchReportConfig);
 router.use(superAdminJwt);
 
 router.post("/add", reportFormatController.addReportFormat);
-router.put("/edit", reportFormatController.addReportFormat);
+router.put("/edit", reportFormatController.updateReportFormat);
 router.delete("/:id", reportFormatController.deleteReportFormat);
 
 export const reportFormatRouter = router;
